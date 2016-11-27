97401c9db828328ae31b051828a6c04996998fb1
ryanmalencia <ryan.malencia@pitt.edu>
2016-11-27 10:03:54 -0500

    Initial Commit

---
+++ Distributor/DistributorLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using DataTypes;
+using WebAPIClient.APICalls;
+
+namespace Distributor
+{
+    public class DistributorLogic
+    {
+        public static void SendJob(Agent agent)
+        {
+            AgentAPI.SetQueued(agent.Name);
+            Thread thread = new Thread(SendTheJob);
+            thread.Start(agent);
+        }
+
+        public static void SendTheJob(object theagent)
+        {
+            Agent agent = (Agent)theagent;
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    client.UploadString("http://" + agent.IP + "/api/machine/give", "");
+                    Console.WriteLine("Sent Job to Agent " + agent.Name);
+                }
+                catch (Exception e)
+                {
+                    AgentAPI.SetDead(agent.Name);
+                    Console.WriteLine("Agent " + agent.Name + " may not be running");
+                }
+            }
+        }
+    }
+}
---
+++ Distributor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using WebAPIClient.APICalls;
+using DataTypes;
+
+namespace Distributor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Distributing jobs...");
+                AgentCollection agents = AgentAPI.GetAllAgents();
+                foreach(Agent agent in agents.machines)
+                {
+                    if (agent.IsIdle())
+                    {
+                        DistributorLogic.SendJob(agent);
+                    }
+                }
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
